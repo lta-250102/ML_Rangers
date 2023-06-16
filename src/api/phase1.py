a8acb504d0cd94eee073c4e71b8810b851fa3da1
Tuan Anh <mravocado102@gmail.com>
2023-06-16 23:37:59 +0700

    save request

--- src/api/phase1.py
+++ src/api/phase1.py
@@ -1,30 +1,30 @@
 import logging
 from api.request import Request
 from api.response import Response
-from core.save_test import save_test
+from fastapi import APIRouter, HTTPException
 from core.phase1 import Prob1Model, Prob2Model
-from fastapi import APIRouter, HTTPException, BackgroundTasks
+from core.save_test import save_test, save_test_executor
 
 
 router = APIRouter()
 logger = logging.getLogger("ml_ranger_logger")
 
 @router.post("/prob-1/predict", response_model=Response)
-async def phase1_prob1(request: Request, background_task: BackgroundTasks) -> Response:
+async def phase1_prob1(request: Request) -> Response:
     try:
         model = Prob1Model()
         response = model.infer(request=request)
-        background_task.add_task(save_test, request, response, 'phase-1', 'prob-1')
+        save_test_executor.submit(save_test, request, response, 'phase-1', 'prob-1')
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
 @router.post("/prob-2/predict", response_model=Response)
-async def phase1_prob2(request: Request, background_task: BackgroundTasks) -> Response:
+async def phase1_prob2(request: Request) -> Response:
     try:
         model = Prob2Model()
         response = model.infer(request=request)
-        background_task.add_task(save_test, request, response, 'phase-1', 'prob-2')
+        save_test_executor.submit(save_test, request, response, 'phase-1', 'prob-2')
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- src/core/save_test.py
+++ src/core/save_test.py
@@ -1,11 +1,14 @@
+from concurrent.futures import ThreadPoolExecutor
 from api.response import Response
 from api.request import Request
 import pandas as pd
 import os
 
 
+save_test_executor = ThreadPoolExecutor(max_workers=1)
+
 def save_test(request: Request, response: Response, phase: str, prob: str):
-    path = os.path.join(os.getcwd(), f'storage/request/phase{phase}_prob{prob}.csv')
+    path = os.path.join(os.getcwd(), f'storage/request/{phase}_{prob}_request_data.csv')
     data = pd.DataFrame(request.rows, columns=request.columns)
     data['label'] = response.predictions
     if os.path.exists(path):

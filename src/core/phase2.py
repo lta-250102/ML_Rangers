1a377e62f8c95aa098a52a7c0ee286c9a8b6a5e8
haydenshimada <nguyendanghuynh1804@gmail.com>
2023-07-28 11:20:29 +0700

    try ordinalEncode

--- src/core/phase2.py
+++ src/core/phase2.py
@@ -7,6 +7,7 @@ import pandas as pd
 from core.model import Model
 from lightgbm import LGBMClassifier
 
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 
 logger = logging.getLogger("ml_ranger_logger")
 
@@ -37,6 +38,41 @@ class Prob2Model(Model):
             'reg_lambda': 0,
         }
         self.model = LGBMClassifier()
+
+    def init_config(self, phase: int, prob: int):
+        self.name = f'phase{phase}_prob{prob}_model'
+        self.logged_model = self.config.get(self.name)
+        self.model_path = self.config.model_dir + '/' + self.name + '.pkl'
+        self.label_encoder_path = self.config.model_dir + '/' + self.name + '_label_encoder.pkl'
+        self.scaler_path = self.config.model_dir + '/' + self.name + '_scaler.pkl'
+        self.train_data_path = self.config.data_dir + f'/phase-{phase}/prob-{prob}/raw_train.parquet'
+        self.features_config : dict = json.loads(open(self.config.data_dir + f'/phase-{phase}/prob-{prob}/features_config.json', 'r').read())
+
+    def load_encoder(self):
+        try:
+            self.label_encoder = pickle.load(open(self.label_encoder_path, 'rb'))
+            self.scaler = pickle.load(open(self.scaler_path, 'rb'))
+        except:
+            logger.info('Encoder not found, creating new one')
+            self.label_encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
+            self.scaler = StandardScaler()
+            
+            data = pd.read_parquet(self.train_data_path, engine='pyarrow')
+            data.drop("label", axis=1, inplace=True)
+            data[self.features_config.get('category_columns', [])] = self.label_encoder.fit_transform(data[self.features_config.get('category_columns', [])])
+            
+            self.scaler.fit(data)
+
+            os.makedirs(self.config.model_dir, exist_ok=True)
+            pickle.dump(self.label_encoder, open(self.label_encoder_path, 'wb'))
+            pickle.dump(self.scaler, open(self.scaler_path, 'wb'))
+
+    def preprocess(self, X: pd.DataFrame) -> pd.DataFrame:
+        X[self.features_config.get('category_columns', [])] = self.label_encoder.transform(X[self.features_config.get('category_columns', [])])
+
+        X = pd.DataFrame(self.scaler.transform(X), columns=X.columns)
+
+        return X
 
     def calculate_drift(self) -> int:
         '''calculate drift'''
